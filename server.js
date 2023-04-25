ef001bb9f9e661fb889b34ef0aaadf648cfc0d95
1da42 <12879533+blok-hamster@users.noreply.github.com>
2023-04-25 07:40:07 -0700

    auto index ord

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 const mongoose = require("mongoose");
 const inscriptRoute = require("./routes/inscriptRoute.js");
 const collectionRoute = require("./routes/collectionRoute.js");
+const interval = process.env.INDEXING_INTERVAL;
+let timerId;
+let blockHeight = 0;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,7 +23,24 @@ app.use((req, res, next) => {
 mongoose.set(`strictQuery`, false);
 mongoose
   .connect(process.env.MONGO_DB_URI)
-  .then((res) => console.log(`Mongo DB Connected! to ${res.connection.host}`))
+  .then((res) => {
+    console.log(`Mongo DB Connected! to ${res.connection.host}`);
+
+    timerId = setTimeout(async function indexOrd() {
+      const blocks = await axios.post(
+        process.env.ORD_API_URL + `/ord/getLatestBlock`
+      );
+      if (blocks.data.message !== `ok`)
+        console.log(
+          `[ALERT]: ORD INDEXING ERROR WITH MESSAGE: ${blocks.data.message}`
+        );
+      if (blocks.data.userResponse.data > blockHeight) {
+        await axios.post(process.env.ORD_API_URL + `/ord/index_ord`);
+        blockHeight = blocks.data.userResponse.data;
+      }
+      timerId = setTimeout(indexOrd, interval);
+    }, interval);
+  })
   .catch(console.error);
 
 app.use(`/api/inscript`, inscriptRoute);
