9215db86b42ab0cae291c6db3a5456c86119b66d
blok-hamster <josicoded@gmail.com>
2023-05-17 23:27:57 +0100

    new branch created

--- routes/inscriptRoute.js
+++ routes/inscriptRoute.js
@@ -3,8 +3,12 @@ const controller = require("../controllers/inscriptionController");
 const path = require("path");
 const router = express.Router();
 const fileUpload = require("express-fileupload");
+const multer = require("multer");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 router.use(express.urlencoded({ extended: false }));
 router.use(express.json());
+
 
 router.use(
   fileUpload({
@@ -14,7 +18,26 @@ router.use(
   })
 );
 
-router.post("/upload", controller.upload);
+
+const storage = multer.diskStorage({
+  destination: async function (req, file, cb) {
+    const directory = `./src/img/uncompressed`;
+    if (!fs.existsSync(directory)) {
+      fs.mkdirSync(directory, { recursive: true });
+    }
+    cb(null, directory);
+  },
+  filename: async function (req, file, cb) {
+    const fileName = new Date().getTime().toString() + path.extname(file.name);
+    cb(null, fileName);
+  }
+});
+
+const upload = multer({
+  storage: storage,
+}).array("unCompImage", 100);
+
+router.post("/upload", upload, controller.upload);
 router.post("/upload/multiple", controller.uploadMultiple);
 router.post("/send/utxo", controller.sendUtxo);
 router.post("/inscribe", controller.inscribe);
